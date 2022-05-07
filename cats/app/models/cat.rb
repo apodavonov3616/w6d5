3962c7e5f6e8a27d41b0ca9e31c249930ba9842c
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-06 22:30:40 -0400

    finished cat class in models

--- cats/app/models/cat.rb
+++ cats/app/models/cat.rb
@@ -1,8 +1,17 @@
 require 'action_view'
 
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
 
-    validates :birth_date, :color, :name, :sex, :description, :
+    validates :birth_date, :name, :color, :sex, presence: true 
 
+    SEX = [M, F]
+    validates :sex, inclusion: SEX
 
+    COLORS = [white, black, blue, striped, brown, bald]
+    validates :color, inclusion: COLORS
+
+    def age
+        time_ago_in_words(:birth_date)
+    end
 end
